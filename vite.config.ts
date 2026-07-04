30cbfdf384fc87a242c7225800c12dca9dbe4768
G-sad-Lux <G-sad-Lux@users.noreply.github.com>
2026-07-03 23:39:37 -0600

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/biblioteca-lux/",
+  base: "/LuxPOO/",
   plugins: [
     react(),
     tailwindcss(),
